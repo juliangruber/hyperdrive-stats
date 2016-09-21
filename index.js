02ac4de84130f80e9f41223c541f5334c3573109
Julian Gruber <julian@juliangruber.com>
2016-09-21 13:23:00 +0200

    emit individual update:* events, add .bytesTotal

--- index.js
+++ index.js
@@ -11,16 +11,29 @@ module.exports = class Stats extends EventEmitter {
     this[_archive] = archive
     this[_stats] = {
       filesProgress: 0,
-      filesTotal: 0
+      filesTotal: 0,
+      bytesTotal: 0
     }
     archive.list({ live: false }).on('data', entry => this.onentry(entry))
+    if (archive.closed) archive.open(() => this.onopen())
+    else setImmediate(() => this.onopen())
   }
   onentry (entry) {
-    this[_stats].filesProgress++
-    this[_stats].filesTotal++
-    this.update()
+    this.update({
+      filesProgress: this[_stats].filesProgress + 1,
+      filesTotal: this[_stats].filesTotal + 1
+    })
   }
-  update () {
+  onopen () {
+    this.update({
+      bytesTotal: this[_archive].content.bytes
+    })
+  }
+  update (data) {
+    for (let key of Object.keys(data)) {
+      this[_stats][key] = data[key]
+      this.emit(`update:${key}`, data[key])
+    }
     this.emit('update', Object.assign({}, this[_stats]))
   }
 }
--- test.js
+++ test.js
@@ -8,14 +8,14 @@ const memdb = require('memdb')
 const drive = hyperdrive(memdb())
 
 test(t => {
+  t.plan(5)
   const archive = drive.createArchive({ live: false })
   archive.createFileWriteStream('file').end('content')
   archive.finalize(() => {
     const stats = new Stats(archive)
-    stats.on('update', s => {
-      t.equal(s.filesProgress, 1)
-      t.equal(s.filesTotal, 1)
-      t.end()
-    })
+    stats.on('update:filesProgress', v => t.equal(v, 1, 'filesProgress'))
+    stats.on('update:filesTotal', v => t.equal(v, 1, 'filesTotal'))
+    stats.on('update:bytesTotal', v => t.equal(v, 7, 'bytesTotal'))
+    stats.on('update', s => t.ok(s, 'update'))
   })
 })
